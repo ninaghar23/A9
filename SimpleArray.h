18606223dc9229de9c715a46632a75ca19e03325
ninaghar23 <nng9@cornell.edu>
2016-11-04 14:03:38 -0400

    Update SimpleArray.h

--- SimpleArray.h
+++ SimpleArray.h
@@ -4,7 +4,7 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-
+#include <exception>
 #include <iomanip>
 using std::setw;
 
@@ -14,15 +14,18 @@ using std::setw;
 // the type is not know yet and will
 // be defined by instantiation
 // of object of class array<T> from main
-template< typename T > class SimpleArray {
+template< typename T, int size > class SimpleArray {
 private:
-    int arraySize;
+    //int arraySize;
     T *arrayStore;
 public:
 // constructor with user pre-defined size
-    SimpleArray (int s) {
-        arraySize = s;
-        arrayStore = new T [arraySize];
+    //~SimpleArray(){
+    //    delete [] arrayStore;
+    //}
+    SimpleArray () {
+        //arraySize = size;
+        arrayStore = new T [size];
     }
 
 // calss array member function to set element of myarray
@@ -32,24 +35,26 @@ public:
     }
 
     T &operator[](int i){
-        try{
-            //if (i >= 0 && i < arraySize){
-            return arrayStore[i];
-            //}
-            //else {
-            //    throw 20;
-            //}
+        //try{
+            if (i < 0 || i >= size){
+                throw std::runtime_error("invalid index");
+            }
+            else {
+                //throw 20;
+                return arrayStore[i];
+            }
         }
-        catch(...)
-        {
-            cout<< "invalid index";
-        }
-        return arrayStore[i];
-    }
+        //catch(...)
+        //{
+        //    cout<< "invalid index";
+        //    arraySt
+        //}
+        //return arrayStore[i];
+    //}
 
 // for loop to display all elements of an array
     void getArray () {
-        for ( int j = 0; j < arraySize; j++ ) {
+        for ( int j = 0; j < size; j++ ) {
 // typeid will retriev a type for each value
             cout << setw( 7 ) << j << setw( 13 ) << arrayStore[ j ]
                  << " type: " << typeid(arrayStore[ j ]).name() << endl;
